69d07c914d3529f41b58deeef12ed0c9ee467642
Jairo Gonzalez <jagonzalez@umariana.edu.co>
2016-09-14 12:24:47 -0500

    codigo

--- prueba14/src/prueba14/mundo.java
+++ prueba14/src/prueba14/mundo.java
@@ -5,6 +5,13 @@ public class mundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//A la pelusa se la follaron todos
+		double base = 5;
+		double exponente =3;
+		int resultado = (int)Math.pow(base, exponente);
+		
+		System.out.println("El resultado de " + "base " + base + " elevado a " + exponente + " es: " + resultado);
+	
 	}
 
+	
 }
